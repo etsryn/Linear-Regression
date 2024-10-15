6dbe3d25829d6ef63d72a9955ac9872ed693ebd8
Rayyan Ashraf <ryshashraf@gmail.com>
2024-10-15 23:17:43 +0530

    linear_prediction.py

--- Predictive Learning Model 1/linear_prediction.py
+++ Predictive Learning Model 1/linear_prediction.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import root_mean_squared_error, r2_score
+
+# To Save the trained model
+# import pickle
 
 train_data = pd.read_excel('train_data.xlsx')
 
@@ -17,8 +20,12 @@ predictions = model.predict(test_data)
 for i in range(len(test_data)):    
     print(f"Input: {test_data.iloc[i].tolist()}, Predicted col3: {predictions[i]}")
 
-mse = mean_squared_error(y_train, model.predict(X_train))
+mse = root_mean_squared_error(y_train, model.predict(X_train))
 print(f"Mean Squared Error (MSE) on Training Data: {mse}")
 
 r2 = r2_score(y_train, model.predict(X_train))
 print(f"R-squared Score (R²) on Training Data: {r2}")
+
+# To Save the trained model
+# with open('linear_regression_model.pkl', 'wb') as model_file:
+    # pickle.dump(model, model_file)
